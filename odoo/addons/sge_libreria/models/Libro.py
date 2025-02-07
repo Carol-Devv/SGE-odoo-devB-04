25dca7263355ed61c28d83fa50e5e6499f33bb1f
Carol-Devv <Carol-Devv@sge.com>
2025-02-07 13:26:12 +0100

    Modelo categoria y libro. Vista from y tree de categoria.

---
+++ odoo/addons/sge_libreria/models/Libro.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class Libro(models.Model):
+    _name = 'sge_libreria.libro'
+    _description = 'Libro'
+    
+    name = fields.Char('Título', required=True)
+    precio = fields.Float('Precio')
+    ejemplares = fields.Integer('Número de ejemplares', help="Número de ejemplares disponibles")
+    fecha_compra = fields.Date('Fecha de compra')
+    segmano = fields.Boolean('Segunda mano')
